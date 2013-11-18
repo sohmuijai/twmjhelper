e50b0427513d1f8cfff092f037b6b052f0f515d8
sohmuijai <vivianwktam@gmail.com>
2013-11-18 19:18:28 +0800

    move actions to fragment

--- src/com/yikwan/mj/twmj/CalculatorFragment.java
+++ src/com/yikwan/mj/twmj/CalculatorFragment.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CalculatorFragment extends Fragment {
+public class CalculatorFragment extends Fragment implements OnClickListener {
 
 	static final String STATE_PULL_SCORES = "statePullScores";
 	ArrayList<Integer> mPullScoresList = new ArrayList<Integer>();
@@ -24,13 +27,43 @@ public class CalculatorFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_calculator, container, false);
+		Log.v("CalcFrag", "onCreateView()");
+		View v = inflater.inflate(R.layout.fragment_calculator, container,
+				false);
+		Button add = (Button) v.findViewById(R.id.button_addScore);
+		add.setOnClickListener(this);
+		Button finish = (Button) v.findViewById(R.id.button_finish);
+		finish.setOnClickListener(this);
+		Button cut = (Button) v.findViewById(R.id.button_cut);
+		cut.setOnClickListener(this);
+		return v;
 	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		Log.v("CalcFrag", "onCreate()");
 		super.onCreate(savedInstanceState);
 		redrawCalcHistory();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		Log.v("CalcFrag", "onSaveInstanceState()");
+		super.onSaveInstanceState(outState);
+		outState.putIntegerArrayList(STATE_PULL_SCORES, mPullScoresList);
+		outState.putInt(STATE_CALC_TOTAL, mTotal);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		Log.v("CalcFrag", "onActivityCreated()");
+		super.onActivityCreated(savedInstanceState);
+		if (savedInstanceState != null) {
+			// Restore last state for checked position.
+			mPullScoresList = savedInstanceState
+					.getIntegerArrayList(STATE_PULL_SCORES);
+			mTotal = savedInstanceState.getInt(STATE_CALC_TOTAL, 0);
+		}
 	}
 
 	public void calcAddScore(View view) {
@@ -92,9 +125,15 @@ public class CalculatorFragment extends Fragment {
 	}
 
 	private void redrawCalcHistory() {
+		Log.v("CalcFrag", "redrawCalcHistory()");
 		TableLayout history_table = (TableLayout) getActivity().findViewById(
 				R.id.history_table);
+		if (null == history_table) {
+			Log.v("", "history_table is null !");
+			return;
+		}
 		TableRow row;
+		Log.v("", "" + mPullScoresList.size());
 		for (int i = 0; i < mPullScoresList.size(); i++) {
 			if (i % 5 == 1) {
 				row = new TableRow(getActivity());
@@ -123,5 +162,20 @@ public class CalculatorFragment extends Fragment {
 		}
 		mPullScoresList.clear();
 		return total;
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.button_addScore:
+			calcAddScore(view);
+			break;
+		case R.id.button_finish:
+			calcFinish(view);
+			break;
+		case R.id.button_cut:
+			calcHalve(view);
+			break;
+		}
 	}
 }
